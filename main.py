ef2269fda23235df9ef397dbf43e36bff59218c4
garvitjain02 <garvitjain02@gmail.com>
2024-04-04 23:08:44 +0530

    Fetch links of all stores in a city

---
+++ main.py
@@ -0,0 +1,25 @@
+# import requests
+# from bs4 import BeautifulSoup
+
+# url = "https://www.lenskart.com/stores/location/bareilly"
+# r = requests.get(url);
+
+# soup = BeautifulSoup(r.content, 'html.parser')
+
+# links = soup.find_all('a', class_="StoreCard_name__mrTXJ")
+# for link in links :
+#     print (link)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.lenskart.com/stores/location/bareilly")
+# driver.implicitly_wait(0.5)
+
+links = driver.find_elements(By.CLASS_NAME, "StoreCard_name__mrTXJ")
+
+for link in links :
+    print (link.get_attribute("href"))
+
+driver.quit()
